a428c943777088bef49c8308ab981bd9615aac18
Pranal Zode <prpzode@gmail.com>
2021-07-09 19:11:17 +0530

    first commit

---
+++ vars.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template('hello.html',list_of_names=['pranal','subham','maitreya'])
+    
+@app.route("/<string:name>")
+def greet(name):
+    return f'Hello {name}'
+
+@app.route("/about")
+def about():
+    return render_template('about.html')
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
